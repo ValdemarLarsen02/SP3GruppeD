dd41f1a7ef6c55eccae7e02f224b7fa21b51cfc4
Jonas-Kestenholz <158442087+Jonas-Kestenholz@users.noreply.github.com>
2024-04-19 10:42:28 +0200

    hh

--- src/Util.java
+++ src/Util.java
@@ -1,20 +1,35 @@
 package src;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Util {
     Scanner scanner = new Scanner(System.in);
 
+    public void displayList(ArrayList<String> list, String msg){
+        System.out.println(msg);
+        for (String option : list) {
+            System.out.println(option);
+        }
+    }
     String promtText(String msg) {
-        return "";
+        displayMsg(msg);
+        String input = scanner.nextLine();
+        return input;
     }
 
     int promptChoice(String msg, ArrayList<String> list){
-
+        displayMsg(msg);
+        displayList(optionslist, "");
+        int input = promptNumeric("");
+        //  return optionslist.get(input-1);
+        return input;
     }
 
     void displayMsg(String msg){
-
+        System.out.println("\n***************");
+        System.out.println(msg);
+        System.out.println("***************\n");
     }
 
 
